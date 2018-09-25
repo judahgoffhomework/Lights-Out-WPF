9a4654dcf38c5a746092ecf55fda7e0d82582683
Judah Goff <bgoff1@harding.edu>
2018-09-25 12:29:55 -0500

    Initial setup for project

---
+++ LightsOut/LightsOut/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LightsOut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LightsOutGame game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            game = new LightsOutGame();
+        }
+
+        private void CreateGrid()
+        {
+            int rectSize = (int)boardCanvas.Width / game.GridSize;
+            // Create rectangles for grid
+            for (int r = 0; r < game.GridSize; r++)
+            {
+                for (int c = 0; c < game.GridSize; c++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = Brushes.White;
+                    rect.Width = rectSize + 1;
+                    rect.Height = rect.Width + 1;
+                    rect.Stroke = Brushes.Black;
+                    // Store each row and col as a Point
+                    rect.Tag = new Point(r, c);
+                    // Register event handler
+                    rect.MouseLeftButtonDown += Rect_MouseLeftButtonDown;
+                    // Put the rectangle at the proper location within the canvas
+                    Canvas.SetTop(rect, r * rectSize);
+                    Canvas.SetLeft(rect, c * rectSize);
+                    // Add the new rectangle to the canvas' children
+                    boardCanvas.Children.Add(rect);
+                }
+            }
+        }
+    }
+}
+}
